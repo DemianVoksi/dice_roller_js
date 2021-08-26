23b64f58472b8ec48c4f816b18dfc17984c48da3
Demian Voksi <demian.voksi@gmail.com>
2021-08-26 21:13:59 +0200

    added html file, added comments, edited functions

--- dice_roller.js
+++ dice_roller.js
@@ -1,28 +1,39 @@
 function howManyDice() {
+  /* Prompts the user to enter the number of dies to roll. */
+
   let numberOfDice = prompt("How many dice do you want to roll? ");
   return Number(numberOfDice);
 }
 
 function whichDie() {
+  /* Prompts the user to enter the type of die to roll. */
+  
   let dice = [4, 6, 8, 10, 12, 20, 100];
-  let die = prompt(`Please enter the number of side you want your die to have. You can choose between ${dice}: `);
+  let die = 0;
+  do {
+    die = prompt(`Please enter the number of side you want your die to have. You can choose between ${dice}: `);
+    die = Number(die);
+  } while (dice.includes(die) == false);
   return die;
-}
+  }
 
 function oneRoll(die) {
-  let dieroll = Math.floor(Math.random() * die);
+  // Calls whichDie() to return a random number between 1 and whichDie()
+
+  let chosenDie = die + 1; // Adds 1 to the chosen die, so the roll can cover the whole spectrum
+  let dieroll = Math.floor(Math.random() * chosenDie);
   if (dieroll == 0) {
-    dieroll = dieroll + 1;
+    dieroll = dieroll + 1; // original roll returns 0, which doesn't exist in the d20 system
     return dieroll;
 } else return dieroll;
 }
 
 function getArray(limit) {
+  /* Calls on howManyDice() in order to get an iterable array 
+  in the length of the number of dice to throw, for later iteration */
+  
   let diceArray = [];
   let i = 1;
   while (diceArray.push(i++) < limit);
   return diceArray;
 }
-
-
-console.log(getArray(howManyDice()))
